38ed4525432e87c4485e731b177b157d94271d9d
RubaCodes <rubacodes@gmail.com>
2022-09-19 14:24:44 +0200

    setter e getter

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Prodotto {
+    //attributi
+    private int codice;
+    private string nome;
+    private string descrizione;
+    private decimal prezzo;
+    private int iva;
+
+    //costruttori
+    //random codice prodotto
+    public Prodotto() {
+        Random r = new Random();
+        this.codice = r.Next();
+    }
+    //Getters
+    //codice solo read
+    public int GetCodice() {
+        return this.codice;
+    }
+    public string GetNome() {
+        return this.nome;
+    }
+    public string GetDescrizione() {
+        return this.descrizione;
+    }
+    public decimal GetPrezzo() {
+        return this.prezzo;
+    }
+    public int getIva() {
+        return this.iva;
+    }
+
+    //Setters
+    public void SetNome(string nome) {
+        this.nome = nome;
+    }
+    public void SetDescrizione(string descrizione) {
+        this.descrizione = descrizione;
+    }
+    public void SetPrezzo(decimal prezzo) {
+        this.prezzo = prezzo;
+    }
+    public void SetIva(int iva) {
+        this.iva = iva;
+    }
+}
