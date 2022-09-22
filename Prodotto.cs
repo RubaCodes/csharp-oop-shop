af292a0c3af8068cf1b4d53fba23f471f987eeab
RubaCodes <rubacodes@gmail.com>
2022-09-22 14:31:57 +0100

    refactoring iniziale classe prodotto

--- Prodotto.cs
+++ Prodotto.cs
@@ -18,4 +18,38 @@
 //Console.WriteLine("---- Codice corretto con pad left di n zeri----");
 //Console.WriteLine(primoProdotto.CodiceConZero());
 #endregion
-Console.WriteLine("Nuovo esercizio");
+public class Prodotto {
+	//attributi
+	public int Codice { get; }
+	public string Nome { get; set; }
+	public string Descrizione { get; set; }
+	public decimal Prezzo { get; set; }
+	public int Iva { get; set; }
+
+	//costruttori
+	//random codice prodotto
+	public Prodotto() {
+		Codice = new Random().Next(9999999);
+	}
+	public Prodotto(int codice) {
+
+		Codice = codice;
+	}
+
+	//metodi
+	public decimal getPrezzoConIva() {
+		return Prezzo * (decimal) (Iva+100)/100;
+	}
+	public string GetNomeEsteso() {
+		return Codice + Nome;
+	}
+
+	public string CodiceConZero() {
+		string codiceString = Convert.ToString(Codice);
+		int diff = 8 - codiceString.Length;
+		for (int i = 0; i < diff; i++) {
+			 codiceString = "0" + codiceString;
+		}
+		return codiceString;
+	}
+}
